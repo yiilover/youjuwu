d66ce11e7434469af36288a36241ee895f191c1e
yiilover <yiilover@gmail.com>
2014-09-19 01:25:39 +0800

    fx

---
+++ uc_server/data/cache/apps.php
@@ -0,0 +1,23 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://www.youjuwu.com',
+    'authkey' => 'HaScNcNeA05062Y3A4Kfs3kcg1LbQ7s0ueA06fs0oei2nbR3p7d7y8yddev8bcJa',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '',
+    'allowips' => '',
+  ),
+);
+
+?>
